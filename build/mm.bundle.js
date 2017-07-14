7e42ad1e150dedcbfa285cdab1b581c7b22d5a66
winidat <winkler_t@gmx.at>
2017-07-14 10:16:00 +0200

    phonegap

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -59967,7 +59967,7 @@ angular.module('mm.core')
     //"demo_sites": {"student": {"url": "http://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "http://school.demo.moodle.net", "username": "teacher", "password": "moodle"}, "cva": {"url": "http://mm.cvaconsulting.com/moodle", "username": "student", "password": "student"}},
     "gcmpn": "694767596569",
     "customurlscheme": "moodlemobile",
-    "siteurl": "school.demo.moodle.net",
+    "siteurl": "https://moodle.common-sense.at/alpha-maroc/",
     //"siteurl": "http://localhost:8080/moodle",
     //"siteurl":  {"url": "http://localhost:8080/moodle", "username": "user", "password": "123456"}}, 
     "skipssoconfirmation": "false",

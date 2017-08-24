5d7878a7665aba6fba3373715ab4869df4139fe3
winidat <winkler_t@gmx.at>
2017-08-24 10:36:28 +0200

    change site

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -59925,7 +59925,7 @@ angular.module('mm.core')
     //"demo_sites": {"student": {"url": "http://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "http://school.demo.moodle.net", "username": "teacher", "password": "moodle"}, "cva": {"url": "http://mm.cvaconsulting.com/moodle", "username": "student", "password": "student"}},
     "gcmpn": "694767596569",
     "customurlscheme": "moodlemobile",
-    "siteurl": "https://onlinecampus-profil.virtuelle-ph.at/",
+    "siteurl": "https://onlinecampus.virtuelle-ph.at/",
     "skipssoconfirmation": "false",
     "forcedefaultlanguage": "false",
     "privacypolicy": "https://moodle.org/mod/page/view.php?id=8148"
